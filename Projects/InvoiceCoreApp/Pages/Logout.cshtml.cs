775602d93828cad6626fc2c84c4e3534c7feba10
e-danz <eduard.danziger@t-online.de>
2025-08-14 16:27:16 +0200

    Logout added

---
+++ Projects/InvoiceCoreApp/Pages/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Threading.Tasks;
+
+namespace InvoiceCoreApp.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Page();
+        }
+    }
+}
